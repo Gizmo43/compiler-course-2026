9349d608feacdceebda6a2cae8d0573a154b81dd
Gizmo43 <egarus2005@gmail.com>
2026-03-19 01:53:58 +0300

    try tests

--- clang/test/compiler-course/krykov_e_c_cast/CastTest.cpp
+++ clang/test/compiler-course/krykov_e_c_cast/CastTest.cpp
@@ -1,21 +1,31 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/krykov_e_c_cast_ClangAST%pluginext -plugin replace_c_cast -fsyntax-only %s | FileCheck %s
+// RUN: split-file %s %t
+// RUN: %clang_cc1 -load %llvmshlibdir/krykov_e_c_cast_ClangAST%pluginext -plugin replace_c_cast %t/test.cpp 2>&1 | FileCheck %s
 
-// CHECK: double b = static_cast<double>(a);
-// CHECK: int *p = reinterpret_cast<int *>(a);
-// CHECK: int *q = const_cast<int *>(&c);
+// CHECK-LABEL: void primitive_tests() {
+// CHECK-NEXT:   int a = 5;
+// CHECK-NEXT:   double b = static_cast<double>(a);
+// CHECK-NEXT:   const int c = 10;
+// CHECK-NEXT:   int *q = const_cast<int *>(&c);
+// CHECK-NEXT:   int *p = reinterpret_cast<int *>(a);
+// CHECK-NEXT: }
 
+// CHECK-LABEL: void user_type_tests() {
+// CHECK-NEXT:   Vec2 v{1,2};
+// CHECK-NEXT:   RawBlock *block = reinterpret_cast<RawBlock *>(&v);
+// CHECK-NEXT:   Child child;
+// CHECK-NEXT:   Parent *p = static_cast<Parent *>(&child);
+// CHECK-NEXT:   Parent *base = new Child();
+// CHECK-NEXT:   Child *c = static_cast<Child *>(base);
+// CHECK-NEXT:   const Vec2 const_v{0,0};
+// CHECK-NEXT:   Vec2 *mutable_v = const_cast<Vec2 *>(&const_v);
+// CHECK-NEXT: }
 
-
+//--- test.cpp
 void primitive_tests() {
   int a = 5;
-  // CHECK: double b = static_cast<double>(a);
   double b = (double)a;
-
   const int c = 10;
-  // CHECK: int *q = const_cast<int *>(&c);
   int *q = (int *)&c;
-
-  // CHECK: int *p = reinterpret_cast<int *>(a);
   int *p = (int *)a;  
 }
 
@@ -40,18 +50,14 @@ public:
 
 void user_type_tests() {
   Vec2 v{1,2};
-  // CHECK: RawBlock *block = reinterpret_cast<RawBlock *>(&v);
   RawBlock *block = (RawBlock *)&v;
 
   Child child;
-  // CHECK: Parent *p = static_cast<Parent *>(&child);
   Parent *p = (Parent *)&child;
 
   Parent *base = new Child();
-  // CHECK: Child *c = static_cast<Child *>(base);
   Child *c = (Child *)base;
 
   const Vec2 const_v{0,0};
-  // CHECK: Vec2 *mutable_v = const_cast<Vec2 *>(&const_v);
   Vec2 *mutable_v = (Vec2 *)&const_v;
 }
